899e1f569a5774690ab1c3b25ca5125a42c8ced0
walid abdelaziz <49992473+walidabdelaziz@users.noreply.github.com>
2023-04-04 12:40:44 +0200

    add constants file

---
+++ Photos Task/Helpers/Consts.swift
@@ -0,0 +1,12 @@
+//
+//  Consts.swift
+//  Photos Task
+//
+//  Created by Walid Ahmed on 04/04/2023.
+//
+
+import Foundation
+class Consts{
+    // MARK:- Photos
+    static var PHOTOS = "https://picsum.photos/v2/list"
+}
